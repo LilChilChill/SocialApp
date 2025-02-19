fd12bc9e9d3ecd4de0b4a7ed0c82053f79eadfe9
LilChilChill <Cuongchi404@gmail.com>
2025-02-19 15:15:31 +0700

    Netlify update deploy

--- Web/Chiap/index.js
+++ Web/Chiap/index.js
@@ -1,3 +1,4 @@
+console.log("API URL:", import.meta.env.VITE_API_URL);
 const API_URL = import.meta.env.VITE_API_URL;
 
 const signUpButton = document.getElementById('signUp');
--- Web/Chiap/vite.config.js
+++ Web/Chiap/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL) || '""',
+  },
 })
